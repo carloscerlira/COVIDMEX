a0cae9a86f20f31fe534e4593ffd9ed0275461a9
carloscerlira <carloscerlira@gmail.com>
2020-04-10 18:52:07 -0500

    update 4/10/20

--- preprocessing.py
+++ preprocessing.py
@@ -4,8 +4,8 @@ import geopandas as gpd
 import matplotlib.pyplot as plt 
 plt.style.use('seaborn')
 
-yestersay = '4/7/20'
-today = '4/8/20'
+yestersay = '4/8/20'
+today = '4/9/20'
 
 confirmedraw = pd.read_csv('data/covidmxraw.csv', skiprows=3, index_col=0)
 confirmedraw = confirmedraw.dropna()
@@ -13,7 +13,6 @@ confirmedraw = confirmedraw[['Unnamed: 1', 'Unnamed: 2', 'Unnamed: 3']]
 confirmedraw = confirmedraw.rename(columns={'Unnamed: 1':'Estado', 'Unnamed: 2':'Sexo', 'Unnamed: 3':'Edad'})
 confirmedraw['Estado'] = confirmedraw['Estado'].apply(lambda name: name[:-1])
 confirmedraw.to_csv('data/confirmedraw.csv')
-print(confirmedraw)
 
 confirmed = pd.read_csv('data/time_series_confirmed_MX.csv', index_col=0)
 current = confirmedraw.groupby('Estado')['Sexo'].count()
@@ -28,7 +27,7 @@ deathsraw = pd.read_csv('data/deathsraw.csv')
 new = deathsraw.groupby('ObservationDay').get_group(today)
 new = new.set_index('Estado')['Deaths']
 deaths[today] = new
-deaths[today][-1]= new.sum()
+deaths[today][-1] = new.sum()
 deaths = deaths.fillna(0)
 deaths.to_csv('data/time_series_deaths_MX.csv')
 
